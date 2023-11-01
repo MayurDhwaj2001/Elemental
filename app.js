e7874922270f418608c830672319a386f52226df
Mayur Dhwaj <mayurdhwaj1315@gmail.com>
2023-11-01 22:51:40 +0530

    db connection

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const bodyparser = require("body-parser");
 const path = require("path");
 // const bool = 0;
 const app = express();
-// app.use(express.static("client/css"));
-// app.use(express.static("client/img"));
+
+const db =
+  "mongodb+srv://mayurdhwaj1315:iGJYGQZDM9RYx261@cluster0.qjvsvxc.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => [console.log(err)]);
 app.use(express.static("client"));
-// app.use(express.static("client/html"));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 
@@ -14,8 +21,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
 
-// app.post("/", function (req, res) {
-// });
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("server started");
